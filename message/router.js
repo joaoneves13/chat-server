5a9d6b1394bc8b5915dd94d08985bf9c25d5a997
João Neves <13joaoneves@gmail.com>
2019-12-13 11:06:41 +0100

    async function in router

--- message/router.js
+++ message/router.js
@@ -1,20 +1,25 @@
-const express = require('express')
-const { Router } = express
-const Message = require('./model')
-const router = new Router()
+const express = require("express");
+const { Router } = express;
+const Message = require("./model");
+const router = new Router();
 
-router.get('/message', (req, res, next) => {
-    Message
-    .findAll()
-    .then(messages => {res.send(messages)})
-    .catch(next)
-})
+router.get("/message", async (req, res, next) => {
+try {
+    const messages = await Message.findAll();
+    res.send(messages);
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.post('/message', (req, res, next) => {
-    Message
-    .create(req.body)
-    .then(message => {res.send(message)})
-    .catch(next)
-})
+router.post("/message", async (req, res, next) => {
+try {
+    const message = await Message.create(req.body);
+    res.send(message);
+  } catch (error) {
+      next(error);
+  }
+});
+  
 
-module.exports = router
+module.exports = router;
